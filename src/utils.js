6763edbf5ecf5b14243fdaee7f6ea5077c46f756
Maxence Godeneche <maxence.godeneche@gmail.com>
2015-11-21 03:44:19 +0100

    Maj Utils.js
    Création de la fonction de génération de mot de passe

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-mailer = require('express-mailer');
+
 Date.prototype.today = function () { 
     return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
 }
